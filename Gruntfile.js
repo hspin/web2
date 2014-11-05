4c557a2b36da848d7c8165b8c62691168c598286
Gerry Salazar <gs@hspindesign.com>
2014-11-05 18:36:21 -0500

    fixed href links

--- Gruntfile.js
+++ Gruntfile.js
@@ -183,6 +183,7 @@ module.exports = function (grunt) {
     // Reads HTML for usemin blocks to enable smart builds that automatically
     // concat, minify and revision files. Creates configurations in memory so
     // additional tasks can operate on them
+              //js: ['concat', 'uglifyjs'],
     useminPrepare: {
       html: '<%= yeoman.app %>/index.html',
       options: {
